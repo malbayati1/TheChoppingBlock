f6241e1362b007b8f439f861de7b50b03963d3e5
malbayati1 <malbayat@uci.edu>
2019-02-21 10:27:12 -0800

    Modified the timer in season Manager so that it doesn't reset when the season changes

--- Assets/Scripts/Managers/SeasonManager.cs
+++ Assets/Scripts/Managers/SeasonManager.cs
@@ -6,12 +6,14 @@ public enum Season { Spring, Summer, Fall, Winter };
 
 public class SeasonManager : Singleton<SeasonManager>
 {
-    public float seasonTimer;
+    public float seasonTimer;   //Should be greater than 0 so the season doesn't change
+                                //right away
 
     public delegate void SeasonChangeDelegate(Season s);
     public event SeasonChangeDelegate seasonChangeEvent = delegate {};
 
-    private float timer;
+    private float timer;    //MA 2/21: timer will continue until the game ends.
+                            //Used to see how long player lasts. May need to change to mintues
     private Season currentSeason;
 
     void Start()
@@ -19,14 +21,11 @@ public class SeasonManager : Singleton<SeasonManager>
         currentSeason = Season.Spring;
     }
 
+    // Muhammad 2/21/2019: Changed the timer
     void Update()
     {
-        timer += Time.deltaTime;
-        if(timer > seasonTimer)
-        {
-            timer = 0;
-            ChangeSeason();
-        }
+        timer += Time.deltaTime; 
+        seasonTimer -= Time.deltaTime;
     }
 
     void ChangeSeason()
