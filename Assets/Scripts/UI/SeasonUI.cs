e1e63160412a81e28e1fff61847e6f80206517e7
EPKgit <ericpaulkim@gmail.com>
2019-02-21 11:27:51 -0800

    scene changes, starting season UI

---
+++ Assets/Scripts/UI/SeasonUI.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SeasonUI : MonoBehaviour
+{
+    public Sprite summerSprite;
+	public Sprite springSprite;
+	public Sprite fallSprite;
+	public Sprite winterSprite;
+
+    void OnEnable()
+    {
+        SeasonManager.instance.seasonChangeEvent += HandleSeasonChange;
+    }
+
+	void OnDisable()
+	{
+		SeasonManager.instance.seasonChangeEvent += HandleSeasonChange;
+	}
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+	void HandleSeasonChange(Season s)
+	{
+		switch(s)
+		{
+			case Season.Spring:
+			{
+
+			} break;
+
+			default:
+			{
+
+			} break;
+		}
+	}
+}
