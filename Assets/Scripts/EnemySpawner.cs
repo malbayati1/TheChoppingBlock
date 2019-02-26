e7e3125131b0e0fc4cfea5a24ea2295eadd0e426
Christian Gramling <gramlinc@uci.edu>
2019-02-25 21:27:20 -0800

    Added SpiderSpawner prefab and enemySpawner script

---
+++ Assets/Scripts/EnemySpawner.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemySpawner : MonoBehaviour
+{
+    public GameObject enemyToSpawnPrefab;
+
+    public float timeBetweenSpawns;
+
+    public int numSpawnsAllowedActive;
+
+		[SerializeField]
+    public List<Vector3> spawnLocations;
+
+    private List<GameObject> activeEnemies;
+
+    private float timeSinceLastSpawn = 0f;
+
+    void Awake()
+    {
+        activeEnemies = new List<GameObject>();
+    }
+
+    void Update()
+    {
+        List<GameObject> livingSpawns = new List<GameObject>();
+        foreach (GameObject go in activeEnemies)
+        {
+            if (go)
+            {
+                livingSpawns.Add(go);
+            }
+        }
+
+        activeEnemies = livingSpawns;
+
+        if (timeSinceLastSpawn > timeBetweenSpawns && activeEnemies.Count < numSpawnsAllowedActive && spawnLocations.Count > 0)
+        {
+            Vector3 location = spawnLocations[Random.Range(0, spawnLocations.Count)];
+            if (Vector3.Distance(location, GameObject.FindWithTag("Player").transform.position) < 5f)
+            {
+                return; //Don't keep trying in case nothing is close
+            }
+						GameObject newEnemy = GameObject.Instantiate(enemyToSpawnPrefab);
+						newEnemy.transform.position = location;
+            activeEnemies.Add(newEnemy);
+            timeSinceLastSpawn = 0f;
+        }
+
+        timeSinceLastSpawn += Time.deltaTime;
+    }
+}
