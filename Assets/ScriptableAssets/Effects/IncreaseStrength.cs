e4b2b8cc3c8b84e09af6dfa616f9c0ed1d0849c6
Keegan Frederick <tefsuncle@gmail.com>
2019-02-26 17:35:00 -0800

    Added playtest scene and adjusted knife and chili prefabs and navmesh prefab.

---
+++ Assets/ScriptableAssets/Effects/IncreaseStrength.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class IncreaseStrength : Effect
+{
+    // public float currentDuration;
+    // public float maxDuration;
+    // public float potency
+
+    public override bool OnApply(PlayerEffects p)
+    {
+        Debug.Log("APPLYING INCREASE STRENGTH");
+        p.stats.strength.AddMultiplicativeModifier(2 + potency * 0.5f);
+        return base.OnApply(p);
+    }
+    public override void OnRemove(PlayerEffects p)
+    {
+        Debug.Log("REMOVING INCREASE STRENGTH");
+        p.stats.strength.RemoveMultiplicativeModifier(2 + potency * 0.5f);
+    }
+}
