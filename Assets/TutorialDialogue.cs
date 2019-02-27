19bc15478b8d6ccd9ef41785d0fe574b93b1645d
Paperboy64 <33039142+KEKissler@users.noreply.github.com>
2019-02-26 23:29:34 -0800

    Add prelim tutorial

---
+++ Assets/TutorialDialogue.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+[RequireComponent(typeof(Text))]
+public class TutorialDialogue : MonoBehaviour
+{
+    public float textCrawlSpeedInSeconds = 0.01f;
+    public Sprite gordon, system;
+    private Text box;
+    private List<string> dialouge = new List<string>();
+    private List<float> delay = new List<float>();
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        box = GetComponent<Text>();
+        dialouge.Add("YO.           \nNEW HIRE.");
+        delay.Add(4);
+        dialouge.Add("Make yourself useful\nand get a pie going!");
+        delay.Add(10);
+
+        StartCoroutine(dialogueAdvancer());
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private IEnumerator dialogueAdvancer()
+    {
+        StartCoroutine(textCrawl(dialouge[0]));
+        yield return new WaitForSeconds(delay[0]);
+        StartCoroutine(textCrawl(dialouge[1]));
+        yield return new WaitForSeconds(delay[1]);
+        Debug.Log("End of Dialogue.");
+        yield return null;
+    }
+
+    private IEnumerator textCrawl(string toAssign)
+    {
+        for (int i = 0; i < toAssign.Length; ++i)
+        {
+            box.text = toAssign.Substring(0, i + 1);
+            yield return new WaitForSeconds(textCrawlSpeedInSeconds);
+        }
+        yield return null;
+    }
+}
