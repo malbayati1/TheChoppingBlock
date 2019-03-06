5011b98aa8545ab5bbaf1faa9e2bc65445a909c4
Paperboy64 <33039142+KEKissler@users.noreply.github.com>
2019-03-05 18:57:12 -0800

    ignore

--- Assets/TutorialDialogue.cs
+++ Assets/TutorialDialogue.cs
@@ -9,6 +9,7 @@ public class TutorialDialogue : MonoBehaviour
     private Text box;
     private List<string> dialouge = new List<string>();
     private List<float> delay = new List<float>();
+    private bool finishedMakingPie = false;
 
     // Start is called before the first frame update
     void Start()
@@ -17,6 +18,8 @@ public class TutorialDialogue : MonoBehaviour
         dialouge.Add("YO.           \nNEW HIRE.");
         delay.Add(4);
         dialouge.Add("Make yourself useful\nand get a pie going!");
+        delay.Add(1);
+        dialouge.Add("Nice, knerd.\nNow put it in the freezer.");
         delay.Add(10);
 
         StartCoroutine(dialogueAdvancer());
@@ -29,6 +32,12 @@ public class TutorialDialogue : MonoBehaviour
         yield return new WaitForSeconds(delay[0]);
         StartCoroutine(textCrawl(dialouge[1]));
         yield return new WaitForSeconds(delay[1]);
+        while (!finishedMakingPie)
+        {
+            yield return new WaitForEndOfFrame();
+        }
+        StartCoroutine(textCrawl(dialouge[2]));
+        yield return new WaitForSeconds(delay[2]);
         Debug.Log("End of Dialogue.");
         yield return null;
     }
@@ -41,5 +50,10 @@ public class TutorialDialogue : MonoBehaviour
             yield return new WaitForSeconds(textCrawlSpeedInSeconds);
         }
         yield return null;
+    }
+
+    public void finishPie()
+    {
+
     }
 }
