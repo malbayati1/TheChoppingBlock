138cd4ada99be7a0d28339bc641554c16a32bb3d
Paperboy64 <33039142+KEKissler@users.noreply.github.com>
2019-03-08 03:24:47 -0800

    Add Char

---
+++ Assets/Scripts/Animation/HeadFallingController.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HeadFallingController : MonoBehaviour
+{
+    public float gravity;
+    public GameObject body;
+
+    private float initOffset;
+
+    void Start()
+    {
+        initOffset = transform.position.y - body.transform.position.y;
+    }
+
+    void Update()
+    {
+        Vector3 nextPosition = transform.position + Vector3.down * gravity * Time.deltaTime;
+        if (nextPosition.y - initOffset < body.transform.position.y)
+        {
+            transform.position = new Vector3(transform.position.x, body.transform.position.y + initOffset, transform.position.z);
+        }
+        else
+        {
+            transform.position = nextPosition;
+        }
+    }
+}
