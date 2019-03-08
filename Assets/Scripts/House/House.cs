dcc6f646489962454f29db904e378da866f2965e
Eric Kim <ericpaulkim@gmail.com>
2019-03-07 16:43:01 -0800

    moving things around house is visually done

---
+++ Assets/Scripts/House/House.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class House : MonoBehaviour
+{
+	private GameObject roof;
+
+	private List<Material> roofMats;
+	private bool inHouse;
+	private float ratio;
+	private Color maxOpacityColor;
+	private Color fullyTransparentColor;
+
+	void Awake()
+	{
+		inHouse = true;
+		ratio = 0;
+		roof = transform.GetChild(1).gameObject;
+		roofMats = new List<Material>();
+		foreach(Transform child in roof.transform)
+		{
+			roofMats.Add(child.gameObject.GetComponent<MeshRenderer>().material);
+		}
+		maxOpacityColor = new Color(roofMats[0].color.r, roofMats[0].color.g, roofMats[0].color.b, 1);
+		fullyTransparentColor = new Color(roofMats[0].color.r, roofMats[0].color.g, roofMats[0].color.b, 0);
+	}
+
+	void Update()
+	{
+		if((inHouse && ratio == 0) || (!inHouse && ratio == 1))
+		{
+			return;
+		}
+		if(inHouse)
+		{
+			ratio -= Time.deltaTime;
+			ratio = Mathf.Clamp(ratio, 0, 1);
+		}
+		else
+		{
+			ratio += Time.deltaTime;
+			ratio = Mathf.Clamp(ratio, 0, 1);
+		}
+		foreach(Material m in roofMats)
+		{
+			m.color = Color.Lerp(fullyTransparentColor, maxOpacityColor, ratio);
+		}		
+	}
+
+    public void Enter()
+	{
+		inHouse = true;
+	}
+
+	public void Exit()
+	{
+		inHouse = false;
+	}
+}
---
+++ Assets/Scripts/House/HouseTrigger.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HouseTrigger : MonoBehaviour
+{
+	public bool isEntrance;
+
+	private House house;
+
+	void Awake()
+	{
+		GameObject parent = gameObject;
+		do
+		{
+			if(house = parent.GetComponent<House>())
+			{
+				return;
+			}
+		} while(parent.transform.parent != null && (parent = parent.transform.parent.gameObject));
+		if(!house)
+		{
+			Debug.Log("HOUSE SCRIPT COULD NOT BE FOUND");
+		}
+	}
+
+    void OnTriggerEnter(Collider col)
+	{
+		if(col.gameObject.CompareTag("Player"))
+		{
+			if(isEntrance)
+			{
+				house.Enter();
+			}
+			else
+			{
+				house.Exit();
+			}
+		}
+	}
+}
