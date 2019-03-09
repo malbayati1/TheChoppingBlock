eb8e4a255cbc568e88d0dc1236a8b8f5f0404519
Eric Kim <ericpaulkim@gmail.com>
2019-03-08 18:18:48 -0800

    slow enemies effect

---
+++ Assets/ScriptableAssets/Effects/SlowEnemies.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class SlowEnemies : Effect
+{
+	// public float currentDuration;
+	// public float maxDuration;
+	// public float potency
+
+	private Dictionary<NavMeshAgent, float> affected;
+
+	public override bool OnApply(PlayerEffects p)
+	{
+		affected = new Dictionary<NavMeshAgent, float>();
+		GameObject[] enemies = GameObject.FindGameObjectsWithTag("Enemy");
+		NavMeshAgent nma;
+		foreach(GameObject g in enemies)
+		{
+			nma = g.GetComponent<NavMeshAgent>();
+			if(nma != null)
+			{
+				affected[nma] = nma.speed;
+				nma.speed /=  (1 + potency);
+			}
+		}
+		return base.OnApply(p);
+	}
+
+	public override void OnRemove(PlayerEffects p)
+	{
+		foreach(KeyValuePair<NavMeshAgent, float> item in affected)
+		{
+			item.Key.speed = item.Value;
+		}
+	}
+}
