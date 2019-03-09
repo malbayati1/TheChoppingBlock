7acd6e4f5d18c57944c4925fdd9018043fd82888
Christian Gramling <gramlinc@uci.edu>
2019-03-09 12:52:28 -0800

    Nerfed enemy spawns and kill enemies on certain seasons

--- Assets/Scripts/Enemies/SwarmingEnemy.cs
+++ Assets/Scripts/Enemies/SwarmingEnemy.cs
@@ -3,10 +3,8 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.AI;
 
-public class SwarmingEnemy : Unit
+public class SwarmingEnemy : BaseEnemy
 {
-    public int damageModifier = 1;
-    public int knockbackModifier = 1;
 	public float preSwarmSpeed;
 	public float swarmSpeed;
 	public bool swarmCreated;
