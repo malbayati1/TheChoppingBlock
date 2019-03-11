880c42198ad4c8a1726e65160a425179f8a3d00f
EPKgit <ericpaulkim@gmail.com>
2019-03-11 13:22:44 -0700

    fix build settings, seperate comparator into new file

--- Assets/Scripts/Interfaces/PerformableAction.cs
+++ Assets/Scripts/Interfaces/PerformableAction.cs
@@ -1,8 +1,4 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-public delegate bool BoolDelegate();
+﻿public delegate bool BoolDelegate();
 public delegate void ActionDelegate();
 
 public class PerformableAction
@@ -15,13 +11,5 @@ public class PerformableAction
 		IsPerformable = bd;
 		priority = p;
 		Action = ad;
-	}
-}
-
-public class PerformableActionComparator : IComparer<PerformableAction>
-{
-	public int Compare(PerformableAction pa1, PerformableAction pa2)
-	{
-		return pa1.priority - pa2.priority;
 	}
 }
---
+++ Assets/Scripts/Interfaces/PerformableActionComparator.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections.Generic;
+
+public class PerformableActionComparator : IComparer<PerformableAction>
+{
+	public int Compare(PerformableAction pa1, PerformableAction pa2)
+	{
+		return pa1.priority - pa2.priority;
+	}
+}
