bb968c3e4232ad4a9901f1cdaa60f4c80c84be72
Paperboy64 <33039142+KEKissler@users.noreply.github.com>
2019-03-11 00:31:30 -0700

    Add teleporter particles

--- Assets/TutorialTeleporter.cs
+++ Assets/TutorialTeleporter.cs
@@ -5,31 +5,42 @@ using UnityEngine.SceneManagement;
 
 public class TutorialTeleporter : MonoBehaviour
 {
-    public Light dirLight;
+    public GameObject player;
+    public GameObject teleportParticleSys;
+
+    private bool startedOnce = false;
+    private ParticleSystem ps;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        ps = teleportParticleSys.GetComponent<ParticleSystem>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if(!startedOnce && Vector3.Distance(player.transform.position, transform.position) < 4.5f)
+        {
+            startedOnce = true;
+            teleportParticleSys.SetActive(true);
+            StartCoroutine(teleportPlayer(5));
+            ps.Play();
+        }
     }
 
-    private void OnTriggerEnter(Collider other)
-    {
-        StartCoroutine(teleportPlayer(10));
-    }
+
+        
+
 
     private IEnumerator teleportPlayer(float delay)
     {
         float cursor = delay;
         while(cursor > 0)
         {
+            Debug.Log(cursor);
             cursor -= Time.deltaTime;
+            yield return new WaitForEndOfFrame();
         }
         SceneManager.LoadScene("PlayTest", LoadSceneMode.Single);
         yield return null;
